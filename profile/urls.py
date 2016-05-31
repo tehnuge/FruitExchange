e6a2e806f04d315b8cf54aa0e3b298b5c8855f42
John Duong <jhnduong@gmail.com>
2016-05-31 16:15:16 -0700

    added url.py hack for view

--- profile/urls.py
+++ profile/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = 'profile'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	#temporary views hack... need to redo this!!!!
+	url(r'^\w+$', views.index, name='detail'),
 	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 	]
+	
