8c6cfc2e3b3271443a407fff4c5e46d7f97638e4
John Duong <jhnduong@gmail.com>
2016-06-15 21:06:47 -0700

    deleted unused models

--- profile/admin.py
+++ profile/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Question
 from .models import Produce
 
-admin.site.register(Question)
 admin.site.register(Produce)
 # Register your models here.
--- profile/models.py
+++ profile/models.py
@@ -3,19 +3,6 @@ from django.conf import settings
 
 from django.db import models
 
-class Question(models.Model):
-	question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date published')
-	def __str__(self):
-		return self.question_text
-
-class Choice(models.Model):
-	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField(default=0)
-	def __str__(self):
-		return self.choice_text
-
 class Produce(models.Model):
 	creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
 	produce_text = models.CharField(max_length=100)
